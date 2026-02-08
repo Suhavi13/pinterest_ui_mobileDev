9be84e8700d4fc02e5356976a5ad9a63aed85ff9
Suhavi13 <suhavikaur@edu.sait.ca>
2026-02-07 23:01:09 -0700

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
-import { Text, View } from "react-native";
-
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
   return (
     <View
