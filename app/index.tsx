48b0956b0761c1d05c6760a401ee85e2c1d6abd4
Suhavi13 <suhavikaur@edu.sait.ca>
2026-02-07 23:03:18 -0700

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -4,14 +4,46 @@ import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+   <SafeAreaView style={styles.container} edges={["top", "bottom"]}>
+      {/* Tabs */}
+      <View style={styles.tabs}>
+        <View style={styles.activeTab}>
+          <Text style={styles.activeText}>For you</Text>
+        </View>
+        <Text style={styles.inactiveText}>Following</Text>
+      </View>
+    </SafeAreaView>
   );
 }
+
+//Styles
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+
+  /* Tabs */
+  tabs: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 16,
+    marginBottom: 10,
+  },
+  activeTab: {
+    backgroundColor: "#000",
+    paddingHorizontal: 18,
+    paddingVertical: 8,
+    borderRadius: 20,
+    marginRight: 14,
+  },
+  activeText: {
+    color: "#fff",
+    fontWeight: "600",
+  },
+  inactiveText: {
+    color: "#000",
+    fontWeight: "600",
+  },
+});
